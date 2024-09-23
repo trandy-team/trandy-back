fcb835b3045e3f5cc607582be793d8a848c407d1
나도관 <9401ndk@t-win.kr>
2024-09-23 10:49:59 +0900

    Github Actions / CodeDeploy 관련 스크립트 파일, 환경변수 추가

---
+++ scripts/stop.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="/home/ubuntu/trandy_server"
+JAR_FILE="$PROJECT_ROOT/build/libs/fittv-api-service-1.0.0-SNAPSHOT.jar"
+
+APP_LOG="$PROJECT_ROOT/deploy_logs/application.log"
+ERROR_LOG="$PROJECT_ROOT/deploy_logs/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy_logs/deploy.log"
+
+TIME_NOW=$(date "+%Y-%m-%d %H:%M:%S")
+
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
+fi
