10f00384ca92954042126011841d5a7c006c4979
나도관 <9401ndk@t-win.kr>
2024-10-11 13:42:55 +0900

    메인 화면 유행 진행 중 게시물 리스트 조회 작업 중 4

--- src/main/java/org/trandy/trandy_server/post/repository/QueryDSL/impl/QueryPostRepositoryImpl.java
+++ src/main/java/org/trandy/trandy_server/post/repository/QueryDSL/impl/QueryPostRepositoryImpl.java
@@ -3,6 +3,7 @@ package org.trandy.trandy_server.post.repository.QueryDSL.impl;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.trandy.trandy_server.comment.domain.QVoteComment;
 import org.trandy.trandy_server.post.domain.Post;
 import org.trandy.trandy_server.post.domain.QPost;
 import org.trandy.trandy_server.post.domain.VoteResult;
@@ -16,10 +17,12 @@ import java.util.List;
 public class QueryPostRepositoryImpl implements QueryPostRepository {
     private final JPAQueryFactory jpaQueryFactory;
     private final QPost post = QPost.post;
+    private final QVoteComment voteComment = QVoteComment.voteComment;
 
     @Override
     public List<Post> retrieveTrendingPostList() {
         List<Post> posts = jpaQueryFactory.selectFrom(post)
+                .join(post.voteComments, voteComment).fetchJoin()
                 .where(post.voteResult.eq(VoteResult.TRENDING),
                         post.deleted.isFalse()
                 )
