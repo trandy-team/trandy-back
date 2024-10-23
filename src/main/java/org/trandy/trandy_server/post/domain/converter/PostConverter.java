e859e520bc05433ab48d5db5d9c7c7f72ef11f93
나도관 <9401ndk@t-win.kr>
2024-10-23 11:23:22 +0900

    DTO 변환 로직 stream -> for문으로 변경

--- src/main/java/org/trandy/trandy_server/post/domain/converter/PostConverter.java
+++ src/main/java/org/trandy/trandy_server/post/domain/converter/PostConverter.java
@@ -3,22 +3,25 @@ package org.trandy.trandy_server.post.domain.converter;
 import org.springframework.stereotype.Component;
 import org.trandy.trandy_server.post.domain.dto.response.PostByCategoryResponse;
 
-import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class PostConverter {
     public List<PostByCategoryResponse> objectToDto(List<Object[]> postList) {
-        return postList.stream()
-                .map(result -> new PostByCategoryResponse(
-                        ((BigInteger) result[0]).longValue(),  // postId
-                        ((Integer) result[1]),                 // voteCount
-                        (String) result[2],                    // nickname
-                        (Boolean) result[3],                   // hasVoted
-                        (String) result[4],                    // voteDeadline
-                        (String) result[5]                     // createdAt
-                ))
-                .collect(Collectors.toList());
+        List<PostByCategoryResponse> result = new ArrayList<>();
+
+        for (Object[] post : postList) {
+            result.add(PostByCategoryResponse.builder()
+                    .postId(Long.parseLong(post[0].toString()))
+                    .voteCount(Integer.parseInt(post[1].toString()))
+                    .nickname(String.valueOf(post[2]))
+                    .hasVoted(Boolean.parseBoolean(post[3].toString()))
+                    .voteDeadline(String.valueOf(post[4]))
+                    .createdAt(String.valueOf(post[5]))
+                    .build());
+        }
+
+        return result;
     }
 }
